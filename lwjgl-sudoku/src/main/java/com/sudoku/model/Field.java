51a7220ab9f08f756589581711753049dea721b0
SimonDamsgaard <Simonpdamsgaard@users.noreply.github.com>
2026-02-25 10:56:48 +0100

    First commit

--- lwjgl-sudoku/src/main/java/com/sudoku/model/Field.java
+++ lwjgl-sudoku/src/main/java/com/sudoku/model/Field.java
@@ -6,12 +6,14 @@ import java.util.List;
 public class Field {
     private int value, x, y;
     private List<Integer> legalEntries = new ArrayList<>();
+    private List<Field> Edges = new ArrayList<>();
 
 
     public Field(int x, int y, int value, int size){
         this.x = x;
         this.y = y;
         this.value = value;
+
         for(int i = 1; i <= size; i++){
             legalEntries.add(i);
         }
@@ -19,6 +21,19 @@ public class Field {
 
     public void setValue(int value){
         this.value = value;
+    }
+    public void addEdge(Field field){
+        Edges.add(field);
+    }
+
+    public void removeEdges(Field field){//This method removes all out going edges
+        Edges.clear();
+    }
+    public void updateLE(int LE){
+        legalEntries.remove(Integer.valueOf(LE));
+    }
+    public void removeEdge(Field field){
+        Edges.remove(field);
     }
 
 
