8a23276cacc1a6793e9479259f3db42a0ba07fd3
Frederik <frederikhalskov@gmail.com>
2026-02-25 11:48:00 +0100

    getvalue

--- lwjgl-sudoku/src/main/java/com/sudoku/model/Field.java
+++ lwjgl-sudoku/src/main/java/com/sudoku/model/Field.java
@@ -20,6 +20,9 @@ public class Field {
     public void setValue(int value){
         this.value = value;
     }
+    public int getValue(int value){
+        return value;
+    }
     public void addEdge(Field field){
         Edges.add(field);
     }
