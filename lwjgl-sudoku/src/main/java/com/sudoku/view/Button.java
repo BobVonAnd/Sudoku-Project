a912e5f7b027a2902b5272fa3f3fa9387962a664
BobVonAnd <s245909@dtu.dk>
2026-03-03 08:57:54 +0100

    .

--- lwjgl-sudoku/src/main/java/com/sudoku/view/Button.java
+++ lwjgl-sudoku/src/main/java/com/sudoku/view/Button.java
@@ -1,11 +1,21 @@
 package com.sudoku.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Button {
     
-    
+    private int value; //the val of Button
+    private List<Integer> LE = new ArrayList<>(); //The Etries that is legal
 
     public Button(){
 
     }
+
+    public void rendering(){
+
+    }
+
+
 
 }
--- lwjgl-sudoku/src/main/java/com/sudoku/view/Windue.java
+++ lwjgl-sudoku/src/main/java/com/sudoku/view/Windue.java
@@ -1,8 +1,15 @@
 package com.sudoku.view;
 
+import java.nio.IntBuffer;
+
+import org.lwjgl.Version;
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
 import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
 import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
 import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
 import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
@@ -22,21 +29,16 @@ import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
 import static org.lwjgl.glfw.GLFW.glfwTerminate;
 import static org.lwjgl.glfw.GLFW.glfwWindowHint;
 import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glClearColor;
-import static org.lwjgl.system.MemoryStack.stackPush;
-
-import java.nio.IntBuffer;
-
-import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
-import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
-import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
-import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWVidMode;
-import org.lwjgl.opengl.GL;
 import org.lwjgl.system.MemoryStack;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 import com.sudoku.App;
 import com.sudoku.model.SudokuBoard;
