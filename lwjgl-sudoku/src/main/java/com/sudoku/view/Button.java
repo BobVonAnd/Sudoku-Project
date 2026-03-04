3cb9be62e4ebf5c30df6ce85dc53a3f7ec24ebcc
BobVonAnd <s245909@dtu.dk>
2026-03-04 10:58:35 +0100

    .

---
+++ lwjgl-sudoku/src/main/java/com/sudoku/view/Button.java
@@ -0,0 +1,19 @@
+package com.sudoku.view;
+
+public class Button {
+
+    private float x,y,hight,width;
+
+    public Button(float x, float y, float hight, float width){
+        this.x = x;
+        this.y = y;
+        this.hight = hight;
+        this.width = width;
+    }
+
+    public void rendering(){
+        glBegin(GL_QUADS)
+
+    }
+    
+}
