651b436197e8e48933456447210a40a1db4e8443
Victor <victor.r2002@gmail.com>
2026-03-11 12:41:15 +0100

    new test info, pulling merge

--- lwjgl-sudoku/src/test/java/com/sudoku/SolverTest.java
+++ lwjgl-sudoku/src/test/java/com/sudoku/SolverTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SolverTest {
     /// We need to test
-    // 
+    // If a cell has 1 LE, check to see if it becomes the value
+    // Make a sudoku board with 1 field remaining, see if it can solve it
+    // All the methods
     
     @Test
     void test() {
