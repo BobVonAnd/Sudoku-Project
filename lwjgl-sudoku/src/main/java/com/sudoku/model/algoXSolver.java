3126f3b733c8615093e8518cd150886019eca40c
SimonDamsgaard <Simonpdamsgaard@users.noreply.github.com>
2026-04-08 12:09:26 +0200

    Removed sparsematrix

--- lwjgl-sudoku/src/main/java/com/sudoku/model/algoXSolver.java
+++ lwjgl-sudoku/src/main/java/com/sudoku/model/algoXSolver.java
@@ -219,37 +219,4 @@ public class algoXSolver {
         }
         return null;
     }
-
-
-
-    public int[][] sparseMatrixCreate(int size){  
-        int constraints = 4;
-        int[][] sparseMatrix = new int[size*size*size][constraints*size*size];
-        int rowIndex = 0;
-
-
-        int boxSize = (int)Math.sqrt(size);
-        for (int i = 0; i < size; i++){
-            for (int j = 0; j<size; j++){
-                for (int n = 0; n < size ; n++){
-
-                    //cell constraint
-                    sparseMatrix[rowIndex][i*size + j] = 1;
-
-                    //row constraint
-                    sparseMatrix[rowIndex][size*size+i*size+n] = 1;
-
-                    //column constraint
-                    sparseMatrix[rowIndex][2*size*size + j*size + n] = 1;
-
-                    //Box constraint
-                    int box = (i/boxSize)*boxSize + (j/boxSize);
-                    sparseMatrix[rowIndex][3*size*size + box*size + n] = 1;
-
-                    rowIndex++;
-                }
-            }
-        }
-        return sparseMatrix;
-    }
 }
