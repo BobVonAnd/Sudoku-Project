ba7b7b52f590fd78248dd66b775766b19b131bdf
BobVonAnd <s245909@dtu.dk>
2026-04-08 12:09:38 +0200

    sat up scenes

--- lwjgl-sudoku/src/main/java/com/sudoku/App.java
+++ lwjgl-sudoku/src/main/java/com/sudoku/App.java
@@ -1,185 +1,16 @@
 package com.sudoku;
-import java.nio.IntBuffer;
 
-import org.lwjgl.Version;
-import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
-import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
-import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
-import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
-import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
-import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
-import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
-import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
-import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
-import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
-import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
-import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
-import static org.lwjgl.glfw.GLFW.glfwPollEvents;
-import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
-import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
-import static org.lwjgl.glfw.GLFW.glfwShowWindow;
-import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
-import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
-import static org.lwjgl.glfw.GLFW.glfwTerminate;
-import static org.lwjgl.glfw.GLFW.glfwWindowHint;
-import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
-import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWVidMode;
-import org.lwjgl.opengl.GL;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
-import static org.lwjgl.opengl.GL11.GL_PROJECTION;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glClearColor;
-import static org.lwjgl.opengl.GL11.glLoadIdentity;
-import static org.lwjgl.opengl.GL11.glMatrixMode;
-import static org.lwjgl.opengl.GL11.glOrtho;
-import org.lwjgl.system.MemoryStack;
-import static org.lwjgl.system.MemoryStack.stackPush;
-import static org.lwjgl.system.MemoryUtil.NULL;
-
-import com.sudoku.model.Field;
-import com.sudoku.model.Solver;
 import com.sudoku.model.SudokuBoard;
 import com.sudoku.model.algoXSolver;
-import com.sudoku.view.Button;
 import com.sudoku.view.TerminalView;
+import com.sudoku.view.Window;
+
 
 public class App {
 
-	// The window handle
-	private long window;
-	private SudokuBoard sudokuBoard;
-
-	public void run() {
-
-		sudokuBoard = new SudokuBoard(9);
-		TerminalView terminalView = new TerminalView(sudokuBoard);
-		sudokuBoard.populate(1);
-		for (int i = 0; i<sudokuBoard.getSize(); i++){//Change method
-			for (int j = 0; j<sudokuBoard.getSize(); j++){
-				Field f = sudokuBoard.getSingleField(i, j);
-				sudokuBoard.makeEdges(f);
-				sudokuBoard.updateLegalEntriesOfField(f);
-			}
-		}
-		Solver solver = new Solver();
-		solver.solves(sudokuBoard);
-		
-		terminalView.printBoard();
-
-
-
-		
-		System.out.println("Running with LWJGL v" + Version.getVersion() + "");
-
-		init();
-		loop();
-
-		// Free the window callbacks and destroy the window
-		glfwFreeCallbacks(window);
-		glfwDestroyWindow(window);
-
-		// Terminate GLFW and free the error callback
-		glfwTerminate();
-		glfwSetErrorCallback(null).free();
-	}
-
-	private void init() {
-		// Setup an error callback. The default implementation
-		// will print the error message in System.err.
-		GLFWErrorCallback.createPrint(System.err).set();
-
-		// Initialize GLFW. Most GLFW functions will not work before doing this.
-		if ( !glfwInit() )
-			throw new IllegalStateException("Unable to initialize GLFW");
-
-		// Configure GLFW
-		glfwDefaultWindowHints(); // optional, the current window hints are already the default
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-		// Create the window
-		window = glfwCreateWindow(1280, 720, "Sudoku", NULL, NULL);
-		if ( window == NULL )
-			throw new RuntimeException("Failed to create the GLFW window");
-
-		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
-		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-		});
-
-		// Get the thread stack and push a new frame
-		try ( MemoryStack stack = stackPush() ) {
-			IntBuffer pWidth = stack.mallocInt(1); // int*
-			IntBuffer pHeight = stack.mallocInt(1); // int*
-
-			// Get the window size passed to glfwCreateWindow
-			glfwGetWindowSize(window, pWidth, pHeight);
-
-			// Get the resolution of the primary monitor
-			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-			// Center the window
-			glfwSetWindowPos(
-				window,
-				(vidmode.width() - pWidth.get(0)) / 2,
-				(vidmode.height() - pHeight.get(0)) / 2
-			);
-		} // the stack frame is popped automatically
-
-		// Make the OpenGL context current
-		glfwMakeContextCurrent(window);
-		// Enable v-sync
-		glfwSwapInterval(1);
-
-		// Make the window visible
-		glfwShowWindow(window);
-	}
-
-	private void loop() {
-		// This line is critical for LWJGL's interoperation with GLFW's
-		// OpenGL context, or any context that is managed externally.
-		// LWJGL detects the context that is current in the current thread,
-		// creates the GLCapabilities instance and makes the OpenGL
-		// bindings available for use.
-		GL.createCapabilities();
-
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity(); 
-		glOrtho(0, 1280, 0, 720, -1, 1); 
-		glMatrixMode(GL_MODELVIEW);
-
-		// Set the clear color
-		glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-		// Run the rendering loop until the user has attempted to close
-		// the window or has pressed the ESCAPE key.
-		while ( !glfwWindowShouldClose(window) ) {
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-		
-			Button button;
-			for (Field[] arrayField : sudokuBoard.getWholeBoard()) {
-				for (Field field : arrayField) {
-					int [] coordinates = field.getCoordinates();
-					button = new Button(coordinates[0], coordinates[1]);
-					button.rendering();
-				}
-			}
-			
-
-			glfwSwapBuffers(window); // swap the color buffers
-			glfwPollEvents();
-		}
-	}
-
 	public static void main(String[] args) {
+		Window window = new Window();
+
 
 		SudokuBoard sudokuBoard = new SudokuBoard(9);
 		sudokuBoard.populate();
@@ -188,20 +19,12 @@ public class App {
 		algoX.algoXManager(sudokuBoard);
 		long endTime = System.nanoTime();
 		long durationOfAlgoX = (endTime - startTime)/1000000;
-		// for (int i = 0; i<sudokuBoard.getSize(); i++){//Change method
-		// 	for (int j = 0; j<sudokuBoard.getSize(); j++){
-		// 		Field f = sudokuBoard.getSingleField(i, j);
-		// 		sudokuBoard.makeEdges(f);
-		// 		sudokuBoard.updateLegalEntriesOfField(f);
-		// 	}
-		// }
-		//Solver solver = new Solver();
-		//solver.solves(sudokuBoard);
-		
+	
 		TerminalView terminalView = new TerminalView(sudokuBoard);
 		terminalView.printBoard();
-		new App().run();
+		
 		System.out.println("The algoX took " + durationOfAlgoX + " miliseconds");
+		window.run();
 	}
-
+	
 }
