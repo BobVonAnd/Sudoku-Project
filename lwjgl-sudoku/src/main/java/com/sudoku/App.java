6fedf113a278741fa4dd37018c9ea1b5eb5d6abe
BobVonAnd <s245909@dtu.dk>
2026-04-08 15:46:59 +0200

    fix

--- lwjgl-sudoku/src/main/java/com/sudoku/App.java
+++ lwjgl-sudoku/src/main/java/com/sudoku/App.java
@@ -6,50 +6,6 @@ import com.sudoku.view.TerminalView;
 import com.sudoku.view.Window;
 import com.sudoku.view.fonts.CreateFont;
 
-public class App {
-
-	// The window handle
-	private long window;
-	private SudokuBoard sudokuBoard;
-
-	public void run() {
-
-		sudokuBoard = new SudokuBoard(9);
-		sudokuBoard.populate(1);
-		long startTime = System.nanoTime();
-		sudokuBoard.solve();
-		long endTime = System.nanoTime();
-		long durationOfAlgoX = (endTime - startTime)/1000000;
-		System.out.println("The algoX took " + durationOfAlgoX + " miliseconds");
-		TerminalView terminalView = new TerminalView(sudokuBoard);
-		terminalView.printBoard();
-		System.out.println("Running with LWJGL v" + Version.getVersion() + "");
-
-		init();
-		loop();
-
-		// Free the window callbacks and destroy the window
-		glfwFreeCallbacks(window);
-		glfwDestroyWindow(window);
-
-		// Terminate GLFW and free the error callback
-		glfwTerminate();
-		glfwSetErrorCallback(null).free();
-	}
-
-	private void init() {
-		// Setup an error callback. The default implementation
-		// will print the error message in System.err.
-		GLFWErrorCallback.createPrint(System.err).set();
-
-		// Initialize GLFW. Most GLFW functions will not work before doing this.
-		if ( !glfwInit() )
-			throw new IllegalStateException("Unable to initialize GLFW");
-
-		// Configure GLFW
-		glfwDefaultWindowHints(); // optional, the current window hints are already the default
-		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
 
 public class App {
 
@@ -58,15 +14,20 @@ public class App {
 		
 		Window window = new Window();
 
-			glfwSwapBuffers(window); // swap the color buffers
-			glfwPollEvents();
-		}
-	}
 
-	public static void main(String[] args) {
+		SudokuBoard sudokuBoard = new SudokuBoard(9);
+		sudokuBoard.populate(1);
+		long startTime = System.nanoTime();
+		algoXSolver algoX = new algoXSolver(); 
+		algoX.algoXManager(sudokuBoard);
+		long endTime = System.nanoTime();
+		long durationOfAlgoX = (endTime - startTime)/1000000;
+	
+		TerminalView terminalView = new TerminalView(sudokuBoard);
+		terminalView.printBoard();
 		
-		new App().run();
-		
+		System.out.println("The algoX took " + durationOfAlgoX + " miliseconds");
+		window.run();
 	}
 	
 }
