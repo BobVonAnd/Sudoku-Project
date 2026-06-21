d448b464a115969d6a7295d65e235e7134b952fb
slayervictor <victor.r2002@gmail.com>
2026-06-21 16:34:44 +0200

    test

--- lwjgl-sudoku/src/main/java/com/sudoku/App.java
+++ lwjgl-sudoku/src/main/java/com/sudoku/App.java
@@ -59,7 +59,7 @@ public class App {
 
 	public void run() {
 
-		sudokuBoard = new SudokuBoard(36);
+		sudokuBoard = new SudokuBoard(9);
 		sudokuBoard.populate(1);
 		long startTime = System.nanoTime();
 		sudokuBoard.solve();
