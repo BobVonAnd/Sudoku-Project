8c6b030f0453af373f6eff452b15d22d07be1ed1
Victor <34196360+slayervictor@users.noreply.github.com>
2026-06-26 20:47:30 +0200

    comments for window, windowinterface, windowmanager

--- lwjgl-sudoku/src/main/java/com/sudoku/controller/WindowInterface.java
+++ lwjgl-sudoku/src/main/java/com/sudoku/controller/WindowInterface.java
@@ -1,11 +1,11 @@
 package com.sudoku.controller;
 
 public interface WindowInterface {
-    public void create();
-    public void step();
+    public void create(); // create gets run once
+    public void step(); // step gets run every frame
 
-    default void keyCallback(int key, int scancode, int action, int mods) {}
-    default void resizeCallback(int width, int height) {}
-    default void mouseButtonCallback(int button, int action, int mods) {}
-    default void cursorPosCallback(double x, double y) {}
+    default void keyCallback(int key, int scancode, int action, int mods) {} // callback, needs to be overridden
+    default void resizeCallback(int width, int height) {} // callback, needs to be overridden
+    default void mouseButtonCallback(int button, int action, int mods) {} // callback, needs to be overridden
+    default void cursorPosCallback(double x, double y) {} // callback, needs to be overridden
 }
