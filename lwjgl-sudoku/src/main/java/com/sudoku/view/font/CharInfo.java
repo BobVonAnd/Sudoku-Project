f868e15b2035e06ec31bfa25ec8330f6fdd24337
BobVonAnd <s245909@dtu.dk>
2026-06-26 13:29:55 +0200

    kommens

--- lwjgl-sudoku/src/main/java/com/sudoku/view/font/CharInfo.java
+++ lwjgl-sudoku/src/main/java/com/sudoku/view/font/CharInfo.java
@@ -2,7 +2,7 @@ package com.sudoku.view.font;
 
 import org.joml.Vector2f;
 
-//Borrowed form GamesWithGabe and edited
+//Borrowed form GamesWithGabe and edited by Nikolaj
 public class CharInfo {
     
     public int x;
@@ -13,18 +13,20 @@ public class CharInfo {
 
     public Vector2f[] textureCoord = new Vector2f[4];
 
-
+    //gets pos and width and height
     public CharInfo(int x, int y, int width, int height){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        // this.yOfSet = 
     }
 
+    //gets a normilized texture coordinate 
     public void calTextureCoord(int fontWidth, int fontHeight){
         float x0 = (float)x / (float)fontWidth;
         float x1 = ((float)x + width) / (float)fontWidth;
+                    //bandage solution. works for fontsize 128 (67.875f)
+                    //make sure that it samples the letter the right way
         float y0 = ((float)y - (height-67.875f)) / (float)fontHeight;
         float y1 = (float)(y+yOfSet) / (float)fontHeight;
 
